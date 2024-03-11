8baa32efa860fc601dde02be078583f38aa716ed
Vit Perov <vitaliy.perov88@gmail.com>
2024-03-11 20:15:31 +0300

    added attachments

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,7 +1,9 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -22,6 +24,14 @@ public class RegistrationTest {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
     @Test
     @Tag("demoqa")
     void successfulRegistrationTest() {
